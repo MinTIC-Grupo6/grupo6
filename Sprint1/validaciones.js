4bde85d91b44d15d47fc8a8f81ef9606a842debe
Juan Leon <skoll79@yahoo.es>
2021-09-21 22:11:46 -0500

    se actualiza con el sprint 2

---
+++ Sprint1/validaciones.js
@@ -0,0 +1,18 @@
+function validar_nombre_usuario(string)
+{
+    //implementacion
+}
+
+function validar_contrasena(string)
+{
+    //implementacion
+}
+
+function confirmar_contrasena(stringA,stringB)
+{
+//implementacion
+}
+
+module.exports.validar_nombre_usuario=validar_nombre_usuario;
+module.exports.validar_contrasena=validar_contrasena;
+module.exports.confirmar_contrasena=confirmar_contrasena;
